3364a8aee61ffeb165e2020b01b51f66d63e03ce
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-06-10 00:34:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (61.65%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,20 +1,58 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        map<int, int> mpp;
+
+        // better sol.
+        // vector<int> ans;
+        // map<int, int> mpp;
+        // int n = nums.size();
+        // int mini = (int) (n/3) + 1;
+
+        // for(int i = 0; i<n; i++){
+        //     mpp[nums[i]]++;
+
+        //     if(mpp[nums[i]] == mini){
+        //         ans.push_back(nums[i]);
+        //     }
+        //     if(ans.size() == 2) break;
+        // }
+        // sort(ans.begin(), ans.end());
+        // return ans; 
+
+        // Optimal Sol.
+
         int n = nums.size();
-        int mini = (int) (n/3) + 1;
+        int cnt1 = 0, cnt2 = 0;
+        int el1  = INT_MIN, el2 = INT_MIN;
 
         for(int i = 0; i<n; i++){
-            mpp[nums[i]]++;
-
-            if(mpp[nums[i]] == mini){
-                ans.push_back(nums[i]);
+            if(cnt1 == 0 && nums[i] != el2){
+                cnt1 = 1, el1 = nums[i];
             }
-            if(ans.size() == 2) break;
+            else if(cnt2 == 0 && nums[i] != el1){
+                cnt2 = 1, el2 = nums[i];
+            }
+            else if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
+
+            else{
+                cnt1--, cnt2--;
         }
+    }
+
+        vector<int> ans;
+        cnt1 = 0, cnt2 = 0;
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] == el1) cnt1++;
+            if(nums[i] == el2) cnt2++;
+        }
+        int mini = (int) (n/3) + 1;
+        if(cnt1 >= mini) ans.push_back(el1);
+        if(cnt2 >= mini) ans.push_back(el2);
+
         sort(ans.begin(), ans.end());
-        return ans; 
+
+        return ans;
     }
 };
