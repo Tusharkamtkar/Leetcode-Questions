1adbea363a304a9d38468b151becd08292235bfc
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-06-13 23:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.22%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -3,24 +3,41 @@ public:
     int maxProduct(vector<int>& nums) {
 
         int n = nums.size();
-        int maximum = INT_MIN;
-        int product = 1;
+        // int maximum = INT_MIN;
+        // int product = 1;
+
+        // for(int i = 0; i<n; i++){
+        //     product = product * nums[i];
+        //     maximum = max(product, maximum);
+
+        //     if(product == 0)
+        //         product = 1;
+        // }
+        //     product = 1;
+        // for(int i = n-1; i >= 0; i--){
+        //     product = product * nums[i];
+        //     maximum = max(product, maximum);
+
+        //     if(product == 0)
+        //     product = 1;
+        // }
+        // return maximum;
+
+        // Another Optimal Solution!
+
+        int prefix = 1;
+        int suffix = 1;
+        int ans = INT_MIN;
 
         for(int i = 0; i<n; i++){
-            product = product * nums[i];
-            maximum = max(product, maximum);
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+            
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-i-1];
 
-            if(product == 0)
-                product = 1;
+            ans = max(ans, max(prefix, suffix));
         }
-            product = 1;
-        for(int i = n-1; i >= 0; i--){
-            product = product * nums[i];
-            maximum = max(product, maximum);
-
-            if(product == 0)
-            product = 1;
-        }
-        return maximum;
+        return ans;
     }
 };
