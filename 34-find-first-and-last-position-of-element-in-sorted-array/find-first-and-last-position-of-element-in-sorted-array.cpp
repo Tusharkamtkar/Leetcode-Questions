18758eceec7ffa9fff037a94cdf851cdfd9f22b8
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-06-17 23:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.78%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,22 +1,47 @@
 class Solution {
+    //   int sp = -1; 
+    //   int ep = -1;
+    //   int n = nums.size();
+
+    //   for(int i = 0 ; i<n; i++){
+    //     if(nums[i] == target){
+    //         sp =i;
+    //         break;
+    //     }
+    //   }
+    //   for(int i = n-1; i>=0; i--){
+    //     if(nums[i] == target){
+    //         ep = i;
+    //         break;
+    //     }
+    //   }
+    //   return {sp, ep};
+
+private: 
+    int lowerBound(vector<int> &nums, int low, int high, int target){
+        while(low  <= high){
+            int mid = (low + high) >> 1;
+
+            if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-      int sp = -1; 
-      int ep = -1;
-      int n = nums.size();
+        int low = 0;
+        int high = nums.size() - 1;
+        int startingPoint = lowerBound(nums, low, high, target);
+        int endPoint = lowerBound(nums, low, high, target + 1) - 1;
 
-      for(int i = 0 ; i<n; i++){
-        if(nums[i] == target){
-            sp =i;
-            break;
+        if(startingPoint < nums.size() && nums[startingPoint] == target){
+            return {startingPoint, endPoint};
         }
-      }
-      for(int i = n-1; i>=0; i--){
-        if(nums[i] == target){
-            ep = i;
-            break;
-        }
-      }
-      return {sp, ep};
+        return {-1, -1};
     }
 };
