df6a636667a90e413a697124da12115f1e787f92
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-04 20:44:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (7.39%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,13 +1,52 @@
 class Solution {
 public:
+
+    bool bs(vector<int> row, int target){
+
+        // BS!
+
+        int low = 0;
+        int high = row.size() - 1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(row[mid] == target){
+                return true;
+            }
+            else if(row[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return false;
+
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-         int n = matrix.size();
-         int m = matrix[0].size();
+        // O(n*m)!
+
+        //  int n = matrix.size();
+        //  int m = matrix[0].size();
+
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         if(matrix[i][j] == target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // O(n) + (log2m)! using BS
+        int n = matrix.size();
+        int m = matrix[0].size();
+
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+            if(matrix[i][0] <= target && matrix[i][m-1] >= target){
+                return bs(matrix[i], target);
             }
         }
         return false;
