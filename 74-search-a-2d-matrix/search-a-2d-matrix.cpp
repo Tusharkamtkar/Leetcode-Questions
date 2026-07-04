513f0b7325818d030f5e57e48d1a2090ab124b17
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-04 21:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (44.64%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
 
-    bool bs(vector<int> row, int target){
+    // bool bs(vector<int> row, int target){
 
-        // BS!
+        // BS! 2nd method
 
-        int low = 0;
-        int high = row.size() - 1;
+    //     int low = 0;
+    //     int high = row.size() - 1;
 
-        while(low <= high){
-            int mid = (low + high) / 2;
+    //     while(low <= high){
+    //         int mid = (low + high) / 2;
 
-            if(row[mid] == target){
-                return true;
-            }
-            else if(row[mid] < target){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
-        }
-        return false;
+    //         if(row[mid] == target){
+    //             return true;
+    //         }
+    //         else if(row[mid] < target){
+    //             low = mid + 1;
+    //         }
+    //         else{
+    //             high = mid - 1;
+    //         }
+    //     }
+    //     return false;
 
-    }
+    // }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // O(n*m)!
+        // O(n*m)! 1st method
 
         //  int n = matrix.size();
         //  int m = matrix[0].size();
@@ -40,13 +40,37 @@ public:
         // }
         // return false;
 
-        // O(n) + (log2m)! using BS
+        // O(n) + (log2m)! using BS 2nd method
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+
+        // for(int i = 0; i<n; i++){
+        //     if(matrix[i][0] <= target && matrix[i][m-1] >= target){
+        //         return bs(matrix[i], target);
+        //     }
+        // }
+        // return false;
+
         int n = matrix.size();
         int m = matrix[0].size();
 
-        for(int i = 0; i<n; i++){
-            if(matrix[i][0] <= target && matrix[i][m-1] >= target){
-                return bs(matrix[i], target);
+        int low = 0;
+        int high = (n * m )- 1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            int row = mid / m;
+            int col = mid % m;
+
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
             }
         }
         return false;
