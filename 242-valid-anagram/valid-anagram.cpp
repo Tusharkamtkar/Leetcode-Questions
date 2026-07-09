acf7069d342f5c3e9e7394c04027838e64dec3b8
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-09 23:29:15 +0530

    Time: 10 ms (7.13%) | Memory: 9.7 MB (76.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.length();
-        int m = t.length();
+        // int n = s.length();
+        // int m = t.length();
 
-        if(n != m) return false;
+        // if(n != m) return false;
 
-        int count[26] = {0};
+        // int count[26] = {0};
 
-        for(int i = 0; i < n; i++){
-            int index = s[i] - 'a';
-            count[index]++;
-        }
-        for(int i = 0; i < m; i++){
-            int index = t[i] - 'a';
+        // for(int i = 0; i < n; i++){
+        //     int index = s[i] - 'a';
+        //     count[index]++;
+        // }
+        // for(int i = 0; i < m; i++){
+        //     int index = t[i] - 'a';
 
-            if(count[index] == 0) return false;
+        //     if(count[index] == 0) return false;
 
-            else count[index]--;
-        }
-        return true;
+        //     else count[index]--;
+        // }
+        // return true;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+       return s == t;
     }
 };
