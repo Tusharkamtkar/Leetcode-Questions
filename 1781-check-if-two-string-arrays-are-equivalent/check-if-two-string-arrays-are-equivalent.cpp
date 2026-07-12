6f120eda78dc31eb2425a91c1c03ca0371d41f75
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-12 22:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (61.25%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-       int n = word1.size();
-       int m = word2.size();
+    //    int n = word1.size();
+    //    int m = word2.size();
 
-       int w1i = 0, i = 0;
-       int w2i = 0, j = 0;      
+    //    int w1i = 0, i = 0;
+    //    int w2i = 0, j = 0;      
        
-       while(w1i < n && w2i < m){
-        if(word1[w1i][i] != word2[w2i][j]){
-            return false;
-        }
-        else{
-            i++;
-            j++;
-        }
-        if(i == word1[w1i].size()){
-            i = 0;
-            w1i++;
-        }
-        if(j == word2[w2i].size()){
-            j = 0;
-            w2i++;
-        }
-       }
-       if(w1i == n && w2i == m) return true;
+    //    while(w1i < n && w2i < m){
+    //     if(word1[w1i][i] != word2[w2i][j]){
+    //         return false;
+    //     }
+    //     else{
+    //         i++;
+    //         j++;
+    //     }
+    //     if(i == word1[w1i].size()){
+    //         i = 0;
+    //         w1i++;
+    //     }
+    //     if(j == word2[w2i].size()){
+    //         j = 0;
+    //         w2i++;
+    //     }
+    //    }
+    //    if(w1i == n && w2i == m) return true;
 
-       return false;
+    //    return false;
+
+    string s1 = "";
+    string s2 = "";
+
+    for(auto ch : word1){
+        s1 += ch;
+    }
+    for(auto ch : word2){
+        s2 += ch;
+    }
+    return s1 == s2;
     }
 };
