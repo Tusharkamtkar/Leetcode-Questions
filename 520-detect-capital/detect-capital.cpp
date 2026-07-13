8485948cc8d8bc881f76984fb5fa184d24f5f4d9
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-13 16:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (80.09%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    bool check(string word , char start, char end){
-        for(auto &ch : word){
-            if(ch < start || ch > end) return false;
-        }
-        return true;
-    }
+    // bool check(string word , char start, char end){
+    //     for(auto &ch : word){
+    //         if(ch < start || ch > end) return false;
+    //     }
+    //     return true;
+    // }
 
     bool detectCapitalUse(string word) {
-        if(check(word, 'A', 'Z') || check(word, 'a', 'z') || check(word.substr(1), 'a', 'z')){
-            return true;
+    //     if(check(word, 'A', 'Z') || check(word, 'a', 'z') || check(word.substr(1), 'a', 'z')){
+    //         return true;
+    //     }
+    //     return false;
+
+    int countCapitals = 0;
+
+    for(char &ch : word){
+        if(isupper(ch)){
+            countCapitals++;
         }
-        return false;
+    }
+    if(countCapitals == 0 || countCapitals == word.size() || (countCapitals == 1 && isupper(word[0]))){
+        return true;
+    }
+    return false;
     }
 };
