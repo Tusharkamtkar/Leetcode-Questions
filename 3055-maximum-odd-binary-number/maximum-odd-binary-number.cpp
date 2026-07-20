50e9763dab8958c1fb08dc302da7816559c337f9
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-20 11:56:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (44.63%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -5,20 +5,16 @@ public:
 
         string result = string(n, '0');
 
-        int count_1 = 0;
-        for(int i = 0; i < n; i++){
-            if(s[i] == '1'){
-                count_1++;
-            }
-        }
+        int count_1 = count(s.begin(), s.end(), '1'); // stl (count)
 
-        int j = 0;
-        while (count_1 > 1) {
-            result[j] = '1';
-            j++;
+        int i = 0;
+        while(count_1 > 1){ // coz one '1' is always belongs to LSB for odd result
+            result[i] = '1';
+            i++;
             count_1--;
         }
-        result[n - 1] = '1';
+        result[n-1] = '1';
+
         return result;
     }
 };
