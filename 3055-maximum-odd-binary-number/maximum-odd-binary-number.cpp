2b7a5e1d9a1906522e76a3b95e846f0fcb8c10a5
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-20 11:53:28 +0530

    Time: 3 ms (23.14%) | Memory: 8.8 MB (77.88%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -5,19 +5,20 @@ public:
 
         string result = string(n, '0');
 
-        int i = 0;
-
-        for(char &ch : s){
-            if(ch == '1'){
-                if(result[n-1] == '1'){
-                    result[i] = '1';
-                    i++;
-                }
-                else{
-                    result[n-1] = '1';
-                }
+        int count_1 = 0;
+        for(int i = 0; i < n; i++){
+            if(s[i] == '1'){
+                count_1++;
             }
         }
+
+        int j = 0;
+        while (count_1 > 1) {
+            result[j] = '1';
+            j++;
+            count_1--;
+        }
+        result[n - 1] = '1';
         return result;
     }
 };
