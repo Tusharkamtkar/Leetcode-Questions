b45bca2ababef385b1ccab58da9123eee9bfff71
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-07-27 10:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (8.09%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> freq;
+
+        for(char &ch : s){
+            freq[ch]++;
+        }
+
+        for(char &ch : t){
+            freq[ch]--;
+
+             if(freq[ch] < 0){
+            return ch;
+        }
+        }
+
+        return 'k';
+    }
+};
