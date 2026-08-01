775aacf50374f9fc65f2dd52866b49346b5d85ad
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-08-01 10:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (92.72%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string,int> mp;
+        
+        unordered_set<string> st;
 
         for(auto &path : paths){
             string source = path[0];
 
-            mp[source] = 1;
+            st.insert(source);
         }
-
         for(auto &path : paths){
             string destination = path[1];
 
-            if(mp[destination] != 1){
+            if(st.find(destination) == st.end()){
                 return destination;
             }
         }
