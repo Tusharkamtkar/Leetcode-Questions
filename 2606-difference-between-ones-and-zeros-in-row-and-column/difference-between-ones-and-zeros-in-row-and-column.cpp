3b99efc116c113208b1627a84c7b4ef63726baf1
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-09-18 20:45:57 +0530

    Time: 25 ms (91.71%) | Memory: 121.8 MB (56.54%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-
+        
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<int> rowOnes(m, 0);
-        vector<int> colOnes(n, 0);
+        vector<int> rowOnes(m,0);
+        vector<int> colOnes(n,0);
 
-        for (int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i++){
 
-            for (int j = 0; j < n; j++) {
+            for(int j =  0; j < n; j++){
 
                 if(grid[i][j] == 1){
-                    
+
                     rowOnes[i] += 1;
                     colOnes[j] += 1;
                 }
             }
         }
 
-        vector<vector<int>> diff(m, vector<int>(n, 0));
+        vector<vector<int>> diff(m, vector<int>(n,0));
 
-        for (int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i++){
 
-            for (int j = 0; j < n; j++) {
+            for(int j = 0; j < n; j++){
+                
+                int onesRow = rowOnes[i];
+                int onesCol = colOnes[j];
+                int zerosRow = n - onesRow;
+                int zerosCol = m - onesCol;
 
-                diff[i][j] = rowOnes[i] + colOnes[j] - (n - rowOnes[i]) -(m - colOnes[j]);
+                diff[i][j] = onesRow + onesCol - zerosRow - zerosCol;
             }
         }
         return diff;
