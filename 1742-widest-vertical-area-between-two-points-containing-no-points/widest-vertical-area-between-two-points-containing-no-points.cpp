deb81046e43491bfa31df7ce9833544cd6a8a98f
Tusharkamtkar <154079593+Tusharkamtkar@users.noreply.github.com>
2026-09-18 20:59:18 +0530

    Time: 35 ms (38.27%) | Memory: 70.4 MB (65.84%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -10,7 +10,9 @@ public:
 
         for(int i = 1; i < n; i++){
 
-            maxArea = max(maxArea, points[i][0] - points[i-1][0]);
+            int area =  points[i][0] - points[i-1][0];
+            
+            maxArea = max(maxArea, area);
         }
         return maxArea;
     }
